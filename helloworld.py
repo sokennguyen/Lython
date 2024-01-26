cc5af2cb7124f149d4c95320bf0fb43c7a626640
soken <nsongkien@gmail.com>
2024-01-26 16:20:25 +0200

    hello

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,5 @@
+import random
+
 print('hello, world')
 myName = input()
 print(myName)
@@ -5,3 +7,9 @@ print('length = ' + str(len(myName)))
 
 spam = True
 print(spam)
+
+for i in range(5):
+    print(i)
+
+for i in range(5):
+    print(random.randint(1, 69))
